9838c1c9d970cc5b5b2358cab3328158ae2c5aa9
Easton Gorishek <ieaston26@gmail.com>
2018-07-13 10:09:14 -0700

    server and db connected

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://localhost:27017/mongoLab';
 let client = null;
 MongoClient.connect(url, { useNewUrlParser: true })
     .then(_client => {

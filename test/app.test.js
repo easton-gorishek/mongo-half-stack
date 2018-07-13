f05763ae6b279170317483a2f29dddc825fac370
carriecarter <carrie@carrie-carter.com>
2018-07-12 17:56:15 -0700

    debugging mongo and server starts

---
+++ test/app.test.js
@@ -0,0 +1,17 @@
+// require('dotenv').config({ path: './test/.env' });
+// const mongo = require('../lib/mongodb');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const app = require('../lib/app');
+
+// chai.use(chaiHttp);
+// const { assert } = chai;
+
+// describe('some API', () => {
+    
+//     it('displays we here', () => {
+//         return chai.request(app);
+       
+
+//     });
+// });

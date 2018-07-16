2982200e79d8448264460cfc651e02ef28470771
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-16 11:20:34 -0700

    Updated request.js for after hook

--- mongo.js
+++ mongo.js
@@ -6,5 +6,5 @@ MongoClient.connect(url, { useNewUrlParser: true })
     .then(_client => {
         client = _client;
         const db = client.db('mongoLab');
-        return db.collection('coffee'); 
+        return db.collection(); 
     });
